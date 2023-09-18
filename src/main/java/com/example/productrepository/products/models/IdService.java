2c8b9bf27e91c5c5e11215775cd70bd6901fbe53
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-18 12:19:46 +0200

    added IdService Classs

---
+++ src/main/java/com/example/productrepository/products/models/IdService.java
@@ -0,0 +1,14 @@
+package com.example.productrepository.products.models;
+import java.util.Random;
+
+public class IdService {
+    private Random random;
+
+    public IdService() {
+        random = new Random();
+    }
+
+    public int generateRandomId() {
+        return random.nextInt();
+    }
+}
